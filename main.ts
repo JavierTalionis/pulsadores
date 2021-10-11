3145cc4349393c92d0c4e170ff6ee9e78936cdf9
Javier Pereira <92329076+JavierTalionis@users.noreply.github.com>
2021-10-11 23:05:48 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let node = 0
 let time = 0
 basic.showString("Inicio")
 radio.setGroup(1)
@@ -6,4 +7,6 @@ basic.forever(function () {
     time = time * 1000000
     control.waitMicros(time)
     basic.showNumber(time / 1000000)
+    node = randint(1, 5)
+    radio.sendNumber(node)
 })
