1e7146a24e6199be8b26654fa20baacf1aae63aa
Javier Pereira <92329076+JavierTalionis@users.noreply.github.com>
2021-10-12 11:17:09 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
-let node = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (node))
+})
 let time = 0
+let node = 0
 basic.showString("Inicio")
 radio.setGroup(1)
 basic.forever(function () {
     time = randint(5, 10)
-    time = time * 1000000
-    control.waitMicros(time)
-    basic.showNumber(time / 1000000)
+    time = time * 1000
+    basic.pause(time)
+    basic.showNumber(time / 1000)
     node = randint(1, 5)
     radio.sendNumber(node)
 })
