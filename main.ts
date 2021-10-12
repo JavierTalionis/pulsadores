6836ccf61af6b049fc3c61e1cf1531b563e60b65
Javier Pereira <92329076+JavierTalionis@users.noreply.github.com>
2021-10-12 11:37:01 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,28 @@
 input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (point))
+})
+input.onButtonPressed(Button.B, function () {
     basic.showString("" + (node))
+})
+radio.onReceivedValue(function (name, value) {
+    if (value == 1) {
+        point += 1
+        basic.showString("" + (point))
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("" + (time / 1000))
 })
 let time = 0
 let node = 0
-basic.showString("Inicio")
+let point = 0
+basic.showString("x")
 radio.setGroup(1)
 basic.forever(function () {
     time = randint(5, 10)
     time = time * 1000
     basic.pause(time)
-    basic.showNumber(time / 1000)
+    soundExpression.hello.play()
     node = randint(1, 5)
     radio.sendNumber(node)
 })
